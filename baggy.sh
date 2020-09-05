bc16a4cd8e301f4ed9dddaf24d17f22345673d00
Sherwyn Moodley <forshish@gmail.com>
2020-09-05 22:38:42 +0200

    Add files via upload

---
+++ baggy.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+clear
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Asset Finder ${reset}"
+echo '----------------------------------------------------------------------'
+cat scope.txt | while read line; do ~/go/bin/assetfinder -subs-only $line | tee -a assets; done
+cat assets | ~/go/bin/httprobe | tee hosts
+cat assets | while read line; do ~/go/bin/gau -subs -providers otx $line; done | tee -a otx
+cat assets hosts otx | sort -u | tee final_urls
+clear
+echo '----------------------------------------------------------------------'
+echo  "${red} Performing : ${green} LFI ${reset}"
+echo '----------------------------------------------------------------------'
+~/go/bin/gf lfi  final_urls | sort -u > lfi
+cat lfi | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/fuzz/lfi -u @ 
+cat lfi | ~/go/bin/nuclei -t ~/nuclei-templates/vulnerabilities/moodle-filter-jmol-lfi.yaml -o lfi_tested
+clear
+echo '----------------------------------------------------------------------'
+echo  "${red} Performing : ${green} REDIRECT TESTING ${reset}"
+echo '----------------------------------------------------------------------'
+~/go/bin/gf redirect final_urls | sort -u > redirect
+cat redirect | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/fuzz/open-redirect/ -u @ 
+export LHOST=$(curl ifconfig.me)
+cat redirect | ~/go/bin/qsreplace "$LHOST" | xargs -I % -P 25 sh -c 'curl -Is "%" 2>&1 | grep -q "Location: $LHOST" && echo "VULN! %"' | tee openredirect.txt
+clear
+echo '----------------------------------------------------------------------'
+echo  "${red} Performing : ${green} CRLF TESTING ${reset}"
+echo '----------------------------------------------------------------------'
+cat final_urls | ~/go/bin/nuclei -t ~/nuclei-templates/vulnerabilities/crlf-injection.yaml -o crlf_tested
+clear
+echo '----------------------------------------------------------------------'
+echo  "${red} Performing : ${green} XSS ${reset}"
+echo '----------------------------------------------------------------------'
+~/go/bin/gf xss final_urls | cut -d : -f3- | sort -u > xss
+cat xss | ~/go/bin/nuclei -c 200 -silent -t ~/nuclei-templates/generic-detections/basic-xss-prober.yaml | tee -a xss_nuclei
+cat xss | ~/go/bin/nuclei -c 200 -silent -t ~/nuclei-templates/generic-detections/top-15-xss.yaml | tee -a xss_nuclei
+cat xss | grep '=' | ~/go/bin/qsreplace a | ~/go/bin/dalfox pipe --silence -b -o dalfox.txt
+clear
+echo '----------------------------------------------------------------------'
+echo  "${red} Performing : ${green} RCE ${reset}"
+echo '----------------------------------------------------------------------'
+~/go/bin/gf rce  final_urls | sort -u > rce
+cat rce | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/cves/ -u @ 
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} CVEs ${reset}"
+echo '----------------------------------------------------------------------'
+cat assets | ~/go/bin/nuclei -c 200 -pbar -t ~/nuclei-templates/cves -o nuclei_cves 
+cat assets | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/cves/ -u @ 
+
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Updating ${reset}"
+echo '----------------------------------------------------------------------'
+sudo apt-get update
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Installing Go, Python, Ruby, git ${reset}"
+echo '----------------------------------------------------------------------'
+sudo apt-get install golang
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install ruby
+sudo apt-get install git
+sudo apt-get install curl
+sudo apt-get install jq
+sudo apt-get install vim
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Installing Go Programs ${reset}"
+echo '----------------------------------------------------------------------'
+go get -u github.com/tomnomnom/anew
+GO111MODULE=on go get -v dw1.io/crlfuzz/cmd/crlfuzz  
+GO111MODULE=on go get -u -v github.com/lc/gau
+go get -u github.com/jaeles-project/gospider
+go get -u github.com/tomnomnom/httprobe
+GO111MODULE=on go get github.com/jaeles-project/jaeles
+git clone https://github.com/projectdiscovery/nuclei.git; cd nuclei/cmd/nuclei/; go build; cp nuclei ~/go/bin/; cd ~;nuclei -h
+GO111MODULE=auto go get -u -v github.com/projectdiscovery/subfinder/cmd/subfinder
+go get github.com/tomnomnom/hacks/waybackurls
+go get -u github.com/tomnomnom/assetfinder
+GO111MODULE=on go get -u -v github.com/hahwul/dalfox
+go get github.com/ffuf/ffuf
+go get -u github.com/tomnomnom/gf
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+go get -u github.com/tomnomnom/qsreplace
+git clone https://github.com/1ndianl33t/Gf-Patterns
+mkdir ~/.gf
+cp ~/Gf-Patterns/* ~/.gf/
+echo 'source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
+git clone https://github.com/projectdiscovery/nuclei-templates nuclei-templates
+git clone --depth=1 https://github.com/jaeles-project/jaeles-signatures /tmp/jaeles-signatures/
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Adding to bashrc ${reset}"
+echo '----------------------------------------------------------------------'
+echo "
+alias ffuf=~/go/bin/ffuf
+alias httpx=~/go/bin/httpx
+alias gau=~/go/bin/gau
+alias anew=~/go/bin/anew
+alias crlfuzz=~/go/bin/crlfuzz
+alias gospider=~/go/bin/gospider
+alias httprobe=~/go/bin/httprobe
+alias nuclei=~/go/bin/nuclei
+alias jaeles=~/go/bin/jaeles
+alias subfinder=~/go/bin/subfinder
+alias dalfox=~/go/bin/dalfox
+alias gf=~/go/bin/gf
+alias qsreplace=~/go/bin/qsreplace
+" >> ~/.bashrc
+source .bashrc
